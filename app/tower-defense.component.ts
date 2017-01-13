e3d0e097eef7ffdf13c33c80cee1e9c0c843f80f
Curtis <curtis@localhost.localdomain>
2017-01-13 09:48:42 +0000

    forgotten files

---
+++ app/tower-defense.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'tower-defense',
+  template: `
+    <div>I am a component</div>
+  `
+})
+
+export class TowerDefenseComponent {
+
+}
